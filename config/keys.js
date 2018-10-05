eeb2e58baa3a8b5bb4b6823dddae0535f22640d9
TinyChief <narsacgm@gmail.com>
2018-10-05 14:58:47 +0300

    api url fixed

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 export default {
   url: process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000/api/'
-    : 'https://fast-woodland-26936.herokuapp.com/'
+    : 'https://fast-woodland-26936.herokuapp.com/api'
     // : 'https://desolate-anchorage-48724.herokuapp.com/api'
 }
